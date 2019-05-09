b3c21362e8c9d947034bb84a7864a2cbe613a30e
hcfinale <3127176962@qq.com>
2019-05-09 09:06:46 +0800

    验证器准备

---
+++ application/common/validate/Column.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\common\validate;
+
+use think\Validate;
+
+class Column extends Validate
+{
+    protected $rule =   [
+        'name'  => 'require|max:25',
+        'age'   => 'number|between:1,120',
+        'email' => 'email',
+    ];
+
+    protected $message  =   [
+        'name.require' => '名称必须',
+        'name.max'     => '名称最多不能超过25个字符',
+        'age.number'   => '年龄必须是数字',
+        'age.between'  => '年龄只能在1-120之间',
+        'email'        => '邮箱格式错误',
+    ];
+
+}
